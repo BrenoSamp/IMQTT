bfe9b8367ce3f7142d30eada9385dc203e244194
Brenim <bsampaio8@hotmail.com>
2022-09-27 00:51:16 -0300

    atualizacoes

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-from paho.mqtt import client as mqtt_client
 import random
+from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
 port = 1883
 topic = "python/mqtt"
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+client_id = f'python-mqtt-{random.randint(0, 100)}'
 username = 'emqx'
 password = 'public'
 
-def connect_mqtt():
+
+def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
+
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
 
 def publish(client):
     msg_count = 0
@@ -35,10 +44,12 @@ def publish(client):
             print(f"Failed to send message to topic {topic}")
         msg_count += 1
 
+
 def run():
     client = connect_mqtt()
-    client.loop_start()
-    publish(client)
+    subscribe(client)
+    client.loop_forever()
+
 
 if __name__ == '__main__':
     run()
