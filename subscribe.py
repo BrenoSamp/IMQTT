2df43a15252055a58be1b9c1ff458fd6b884d1b9
Brenim <bsampaio8@hotmail.com>
2022-09-27 01:27:13 -0300

    atualizacoes

--- subscribe.py
+++ subscribe.py
@@ -1,4 +1,5 @@
 import random
+import time
 from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
@@ -29,20 +30,6 @@ def subscribe(client: mqtt_client):
 
     client.subscribe(topic)
     client.on_message = on_message
-
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
 
 
 def run():
